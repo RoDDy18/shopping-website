b2e07e1606d0d0285586b11ea1bb69ef9982eaa3
Emmnauel Oni <damilolaoni2004@gmail.com>
2022-06-03 14:41:21 +0100

    update side Navigation

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -3,18 +3,16 @@ app.component("side-nav", {
     `
     <div class="side-nav">
         <ul id="categories">
-        <li><a href="">Grocery<i class="fa fa-apple"></i> </a></li>
-        <li><a href="">Health & Beauty</a></li>
-        <li><a href="">Home & Office<i class="fa fa-building"></i></a></li>
-        <li><a href="">Phones & Tablets<i class="fa fa-mobile"></i></a></li>
-        <li><a href="">Computing<i class="fa fa-laptop"></i></a></li>
+        <li><a href="">Beauty</a></li>
+        <li><a href="">Home & Office</a></li>
+        <li><a href="">Phones & Tablets</a></li>
+        <li><a href="">Computing</a></li>
         <li><a href="">Electronics</a></li>
         <li><a href="">Fashion</a></li>
-        <li><a href="">Baby Products</a></li>
         <li><a href="">Gaming</a></li>
         <li><a href="">Sporting Goods</a></li>
         </ul>
     </div>
     `,
-    data:()=>{return {}}
+    data:()=>{return {}},
 })
